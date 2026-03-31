28716ee072c9d7fdf054111b9b84e06b69b157da
Andrii Forkaliuk <aforkalyuk@gmail.com>
2026-03-31 16:56:30 +0300

    Release 0.0.8

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdtelligentSwiftSDK",
-            url: "https://github.com/Adtelligent/AdtelligentSwiftSDK/releases/download/0.0.7/AdtelligentSwiftSDK.xcframework.zip",
-            checksum: "597a540fef7b857c5336bc11d534785abf48f0550932ae526c88b0c129164942"
+            url: "https://github.com/Adtelligent/AdtelligentSwiftSDK/releases/download/0.0.8/AdtelligentSwiftSDK.xcframework.zip",
+            checksum: "562483cdf9bf265e5b5732aee224655a6543d473ce67c24e447f817a1ef23786"
         ),
         .testTarget(
             name: "AdtelligentSwiftSDKTests",
