7a90ff56c6a95705e2ec0aa500972c041fa3c607
Andrii Forkaliuk <aforkalyuk@gmail.com>
2026-07-19 14:21:54 +0300

    Remove test target — tests now live in AdtelligentSwiftSDKCore

--- Package.swift
+++ Package.swift
@@ -17,9 +17,5 @@ let package = Package(
             url: "https://github.com/Adtelligent/AdtelligentSwiftSDK/releases/download/0.1.0/AdtelligentSwiftSDK.xcframework.zip",
             checksum: "e696a4b12246cda900833759cf4b3d8f8dc01aa8fa374269814bf78da1c840fa"
         ),
-        .testTarget(
-            name: "AdtelligentSwiftSDKTests",
-            dependencies: ["AdtelligentSwiftSDK"]
-        ),
     ]
 )
